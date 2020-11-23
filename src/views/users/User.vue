ce5ce947ffb2af6ea5161c4370b7c46868a5ca2c
Abhishekkochar <abhishekkochar2@gmail.com>
2020-11-24 08:48:00 +1300

    Starker translation test
    Starker translation test

---
+++ src/views/users/User.vue
@@ -0,0 +1,56 @@
+<template>
+  <CRow>
+    <CCol col="12" xl="8">
+      <CCard>
+        <CCardHeader>
+          Posts
+        </CCardHeader>
+        <CCardBody>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="post in posts" :key="post.userId">
+                <th scope="row">{{post.userId }}</th>
+                <span @mouseover="hover = true" @mouseleave="hover = false">
+                <td>{{post.title }}</td>
+                 </span>
+                 <span v-if="hover">
+                <td>{{post.body }}</td>
+                </span>
+              </tr>
+            </tbody>
+          </table>
+
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default{
+    name:'User',
+    data(){
+      return{
+        hover:false,
+        posts: null,
+      };
+    },
+    created: function(){
+      axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res=>{
+        this.posts = res.data;
+      })
+    },
+     
+  }
+
+</script>
---
+++ src/views/users/Users.vue
@@ -0,0 +1,52 @@
+<template>
+  <CRow>
+    <CCol col="12" xl="8">
+      <CCard>
+        <CCardHeader>
+          Users
+        </CCardHeader>
+        <CCardBody>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">City</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in users" :key="user.id" v-on:click="nextPage()">
+                <th scope="row">{{user.id }}</th>
+                <td>{{user.name }}</td>
+                <td>{{user.email }}</td>
+                <td>{{user.address.city }}</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default{
+    name:'Users',
+    data(){
+      return{
+        users: null,
+      };
+    },
+    created: function(){
+      axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res=>{
+        this.users = res.data;
+      })
+    },
+     
+  }
+</script>
